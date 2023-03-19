57f72a751d22ced4ab05dc3657db67bc1d59786e
SirKilika <fabiomacielsilva87@gmail.com>
2023-03-19 18:08:44 +0000

    2.1

--- globomantics/store/urls.py
+++ globomantics/store/urls.py
@@ -6,6 +6,7 @@ from .views import ElectronicsView,EquipmentView
 urlpatterns = [
     path('', views.index, name = "index"),
     re_path(r'^\d+', views.detail, name="detail"),
-    #re_path(r'^electronics', views.electronics, name='electronics'),
-    re_path(r'^electronics',EquipmentView.as_view(), name='electronics')
+    re_path(r'^electronics', views.electronics, name='electronics'),
+    re_path(r'^logout',views.logout, name='logout'),
+    #re_path(r'^electronics',EquipmentView.as_view(), name='electronics')
 ]
--- globomantics/store/views.py
+++ globomantics/store/views.py
@@ -15,6 +15,14 @@ def index(request):
 def detail(request):
     return HttpResponse("This is a detail page")
 
+def logout(request):
+    try:
+        del request.session['customer']
+    except KeyError:
+        print("Error while logging out")
+    return HttpResponse("You're logged out")
+
+
 @require_http_methods(["GET"])
 @csrf_exempt
 def electronics(request):
@@ -22,15 +30,28 @@ def electronics(request):
     if request.method == 'GET':
         paginator = Paginator(items,2)
         pages= request.GET.get('page',1)
+        name = "Fabio"
         try:
             items = paginator.page(pages)
         except PageNotAnInteger:
             items = paginator.page(1)
         except EmptyPage:
             items = paginator.page(paginator.num_pages)
-        return render(request, 'store/list.html',{'items': items})
+        if not request.session.has_key('customer'):
+            request.session['customer'] = name
+            print("Session value set")
+        response = render(request, 'store/list.html',{'items': items})
+        if request.COOKIES.get('visits'):
+            value = int(request.COOKIES.get('visits'))
+            print("Getting Cookie")
+            response.set_cookie('visits', value + 1)
+        else:
+            value = 1
+            print("Setting Cookie.")
+            response.set_cookie('visits', value)
+        return response    
     elif request.method == "POST":
-        return HttpResponseNotFound("POST method is not allowed")
+        return HttpResponseNotFound("Page Not Found")
     
 class ElectronicsView(View):
     def get(self, request):
